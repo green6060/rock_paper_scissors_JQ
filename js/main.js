56d0fa31aa4fd37be4a9cc6672e022c628591f63
parkerprecisionweb <dallasjamesparker@gmail.com>
2018-08-21 17:27:01 -0600

    fix bug selecting appended items add random computer choice add comparison loop

--- js/main.js
+++ js/main.js
@@ -4,33 +4,71 @@
 // the computer's choice will be compared against the user's choice
 // the result will be displayed, and added to the user's history
 // average win choice percentage is calculated
-
-$(document).ready( function(){
-
-  var choiceArray = {rock: 'rock', paper:'paper', scissors:'scissors'};
+$(document).ready( function() {
+  var choiceArray = ["rock", "paper", "scissors"];
+  var winCount = 0;
+  var lossCount = 0;
+  var drawCount = 0;
 
 // clicking on start button REPLACES it with 3 new buttons, "rock" "paper" "scissors"
   $('#strtBtn').click(function() {
     $('#strtBtn').remove();
-    $( "#buttons" ).append( "<button id='rockBtn'>Rock</button>" );
-    $( "#buttons" ).append( "<button id='rockBtn'>Rock</button>" );
-    $( "#buttons" ).append( "<button id='rockBtn'>Rock</button>" );
+    $( "#buttons" ).append( '<button id="rockBtn">Rock</button>' ); 
+    $( "#buttons" ).append( '<button id="paperBtn">Paper</button>' );
+    $( "#buttons" ).append( '<button id="scissorsBtn">Scissors</button>' );
     }
   );
 
-  $('#rockBtn').click(function() {
 
+
+  $('#buttons').on('click','#rockBtn', function() {
+
+    var compRand = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+      
+    if (compRand == 'rock') {
+      drawCount += 1;
+
+    } else if (compRand == 'paper') {
+      lossCount += 1;
+
+    } else if (compRand == 'scissors') {
+      winCount += 1;
+      
+    } else {
+      //some error
+      alert("error!")
     }
-  );
-
-  $('#rockBtn').click(function() {
-
+    
+  });
+  
+  $('#buttons').on('click','#paperBtn', function() {
+    var compRand = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    
+    if (compRand == 'rock') {
+      winCount += 1;
+    } else if (compRand == 'paper') {
+      drawCount += 1;
+    } else if (compRand == 'scissors') {
+      lossCount += 1;
+    } else {
+      //some error
     }
-  );
-
-  $('#rockBtn').click(function() {
-
+  
+  });
+  
+  $('#buttons').on('click','#scissorsBtn', function() {
+    var compRand = choiceArray[Math.floor(Math.random()*choiceArray.length)];
+    
+    if (compRand == 'rock') {
+      lossCount += 1;
+    } else if (compRand == 'paper') {
+      winCount += 1;
+    } else if (compRand == 'scissors') {
+      drawCount += 1;
+    } else {
+      //some error
     }
-  );
+
+  });
 
 })
